f97e48ac4567ccc377ebf2c3e625e0d98346790d
Sudhir Yadav <sudhiryadav347@gmail.com>
2022-04-19 19:35:23 +0530

    Adding global colors and Editing Navigation

--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -1,14 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 const AllExpenses = () => {
   return (
     <View>
       <Text>AllExpenses</Text>
     </View>
-  )
-}
+  );
+};
 
-export default AllExpenses
+export default AllExpenses;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,14 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
 const RecentExpenses = () => {
   return (
     <View>
       <Text>RecentExpenses</Text>
     </View>
-  )
-}
+  );
+};
 
-export default RecentExpenses
+export default RecentExpenses;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
