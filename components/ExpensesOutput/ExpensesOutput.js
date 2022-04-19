39195346f3c625dfacf4c700621a0492274c2c98
Sudhir Yadav <sudhiryadav347@gmail.com>
2022-04-20 04:07:59 +0530

    Adding messgage when nno expense item was found for recent and all expenses screens.

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -1,13 +1,19 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import ExpensesList from './ExpensesList';
 import ExpensesSummary from './ExpensesSummary';
 import { GlobalStyles } from '../../constants/styles';
 
-const ExpensesOutput = ({ expenses, expensesPeriod }) => {
+const ExpensesOutput = ({ expenses, expensesPeriod, fallbackText }) => {
+
+  let content = <Text style={styles.infoText}>{fallbackText}</Text>;
+
+  if(expenses.length > 0) {
+     content = <ExpensesList expenses={expenses} />;
+  }
   return (
     <View style={styles.container}>
       <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
-      <ExpensesList expenses={expenses} />
+      {content}
     </View>
   );
 };
@@ -22,4 +28,10 @@ const styles = StyleSheet.create({
     paddingBottom: 0,
     backgroundColor: GlobalStyles.colors.primary700,
   },
+  infoText: {
+    color: 'white',
+    fontSize: 16,
+    textAlign: 'center',
+    marginTop: 32
+  }
 });
--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -6,7 +6,7 @@ import { ExpensesContext } from '../store/expenses-context';
 const AllExpenses = () => {
   const expensesCtx = useContext(ExpensesContext);
   return (
-    <ExpensesOutput expenses={expensesCtx.expenses} expensesPeriod="Total" />
+    <ExpensesOutput expenses={expensesCtx.expenses} expensesPeriod="Total" fallbackText="No registered expenses found!"/>
   );
 };
 
